62345dfa44040d43334ac5b4c9ae593a19ab7524
fernandobloedorn <fernandobloedorn@gmail.com>
2020-03-31 15:47:02 -0300

    Updates

---
+++ precache-manifest.7a9bc88a1ca9afcbbb536d4e5fa0337d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88227c20233fb71b9c13410f9e9b2adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b09c2ee394c15691e3",
+    "url": "/static/css/main.1c25d7b7.chunk.css"
+  },
+  {
+    "revision": "91b76eccb112ff4087d9",
+    "url": "/static/js/2.d428bf0c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d428bf0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b09c2ee394c15691e3",
+    "url": "/static/js/main.136b9aaa.chunk.js"
+  },
+  {
+    "revision": "a0ed782dc191e357cf6c",
+    "url": "/static/js/runtime-main.7e8b41f1.js"
+  }
+]);
