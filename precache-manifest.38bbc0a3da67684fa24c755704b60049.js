a410c51b5dcdb78e49ecfdb5b03f4bcd32673736
fernandobloedorn <fernandobloedorn@gmail.com>
2020-04-02 22:10:54 -0300

    Initial commit

---
+++ precache-manifest.38bbc0a3da67684fa24c755704b60049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab561a377eed9d51813453e30f003cac",
+    "url": "/fernandobloedorn/lista-produtos/index.html"
+  },
+  {
+    "revision": "0b3c32ab4aacb83dffc7",
+    "url": "/fernandobloedorn/lista-produtos/static/css/main.1c25d7b7.chunk.css"
+  },
+  {
+    "revision": "91b76eccb112ff4087d9",
+    "url": "/fernandobloedorn/lista-produtos/static/js/2.d428bf0c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fernandobloedorn/lista-produtos/static/js/2.d428bf0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3c32ab4aacb83dffc7",
+    "url": "/fernandobloedorn/lista-produtos/static/js/main.94916b54.chunk.js"
+  },
+  {
+    "revision": "6dc8c662eb1bcde4c12e",
+    "url": "/fernandobloedorn/lista-produtos/static/js/runtime-main.f159498a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56fb7a0e40903eeb7c5805952fb66a71.js"
+  "/fernandobloedorn/lista-produtos/precache-manifest.38bbc0a3da67684fa24c755704b60049.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fernandobloedorn/lista-produtos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
